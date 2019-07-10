2bb4a086c92f5f93f314a28d6bed1bbde4fa45c9
Bob Forgey <sesamemucho@gmail.com>
2019-07-10 06:24:54 -0700

    Now almost running udpsender from threads.

--- src/udpsender/sender.py
+++ src/udpsender/sender.py
@@ -1,11 +1,36 @@
 """Sends UDP packets on a schedule.
 """
 
-import pprint
+import datetime
+import threading
+import time
 import typing
 
 from udpsender import config
 
+class UDPSrunner(threading.Thread):
+    def __init__(self,
+                run_request: threading.Event,
+                schedule: config.UDPSSchedule):
+
+        super().__init__(name=schedule.name)
+        self.schedule = schedule
+        self.run_request = run_request
+        self.quitquit = threading.Event()
+
+    def quit(self):
+        self.quitquit.set()
+
+    def run(self):
+        self.run_request.wait()
+        this_time = datetime.datetime.utcnow()
+        i = 0
+        time.sleep(self.schedule.delay)
+        while not self.quitquit.is_set():
+            print(f"Hello from {self.name}")
+            i += 1
+            if i >= 5:
+                self.quitquit.set()
 
 class UDPSender:
     def __init__(self) -> None:
@@ -14,3 +39,14 @@ class UDPSender:
     def run(self, stream: typing.TextIO) -> None:
         schedules = config.get_schedules(stream)
         print(f"schedule: {schedules[0]}")
+        syncthreads = threading.Event()
+        sch_threads = list()
+        for sched in schedules:
+            sth = UDPSrunner(syncthreads, sched)
+            sch_threads.append(sth)
+            sth.start()
+
+        syncthreads.set()
+
+        for sth in sch_threads:
+            sth.join()
